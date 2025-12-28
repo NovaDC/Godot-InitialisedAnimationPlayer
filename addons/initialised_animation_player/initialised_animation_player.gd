5b2b5a3d4ba035e78609b3162cab3d28a0093e05
NovaDC <164206531+NovaDC@users.noreply.github.com>
2025-12-28 13:59:18 -0600

    Add editor configuration warnings

--- addons/initialised_animation_player/initialised_animation_player.gd
+++ addons/initialised_animation_player/initialised_animation_player.gd
@@ -10,7 +10,12 @@ extends AnimationPlayer
 ## if the specified animation exists.
 
 ## The name of the animation to play.
-@export var initial_animation:StringName = "RESET"
+@export var initial_animation:StringName = "RESET":
+	get:
+		return initial_animation
+	set(_value):
+		initial_animation = _value
+		update_configuration_warnings()
 
 ## A custom blend value to play the [member initial_animation] with.
 @export var custom_blend:float = -1
@@ -23,8 +28,15 @@ extends AnimationPlayer
 
 func _ready():
 	play_initial_animation()
+	animation_libraries_updated.connect(update_configuration_warnings)
+	animation_list_changed.connect(update_configuration_warnings)
 
 ## Plays the [member initial_animation] as specified.
 func play_initial_animation():
 	if has_animation(initial_animation):
 		play(initial_animation, custom_blend, custom_speed, from_end)
+
+func _get_configuration_warnings() -> PackedStringArray:
+	if get_animation_list().is_empty() or has_animation(initial_animation):
+		return PackedStringArray()
+	return PackedStringArray(["Inital animation named '%s' could not be found." % [initial_animation]])
