11b7b722597a3a0afa8732efd76f42192b081898
NovaDC <164206531+NovaDC@users.noreply.github.com>
2025-12-28 14:10:45 -0600

    Optimize and upgrade icon

--- addons/initialised_animation_player/initialised_animation_player.gd
+++ addons/initialised_animation_player/initialised_animation_player.gd
@@ -1,5 +1,5 @@
 @tool
-@icon("res://addons/initialised_animation_player/initialised_animation_player.svg")
+@icon("res://addons/initialised_animation_player/icon.svg")
 class_name InitialisedAnimationPlayer
 extends AnimationPlayer
 
--- addons/initialised_animation_player/plugin.gd
+++ addons/initialised_animation_player/plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 const PLUGIN_NAME := "InitialisedAnimationPlayer"
 
-const PLUGIN_ICON := preload("res://addons/initialised_animation_player/initialised_animation_player.svg")
+const PLUGIN_ICON := preload("res://addons/initialised_animation_player/icon.svg")
 
 func _get_plugin_name() -> String:
 	return PLUGIN_NAME
